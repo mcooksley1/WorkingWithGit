fc02cbfb408dda3d719045554781da618161e844
Mr Mason Cooksley <mcn@gmail.com>
2022-11-16 14:44:02 +0000

    Commit staged

--- WorkingWithGit/WorkingWithGit/Program.cs
+++ WorkingWithGit/WorkingWithGit/Program.cs
@@ -10,7 +10,6 @@ namespace WorkingWithGit
             Console.WriteLine("i just added this in the Visual Studio");
             Console.WriteLine("i just added this in the command line repo");
             Console.WriteLine("This is added to show the staging area");
-            Console.WriteLine("This is an additional line");
         }
     }
 }
