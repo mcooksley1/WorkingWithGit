3a588e9671f378638f53bd7a1f14bc2a8a258fa3
Mr Mason Cooksley <mcn@gmail.com>
2022-11-16 14:41:59 +0000

    First Commit

--- WorkingWithGit/WorkingWithGit/Program.cs
+++ WorkingWithGit/WorkingWithGit/Program.cs
@@ -9,6 +9,8 @@ namespace WorkingWithGit
             Console.WriteLine("Hello World!");
             Console.WriteLine("i just added this in the Visual Studio");
             Console.WriteLine("i just added this in the command line repo");
+            Console.WriteLine("This is added to show the staging area");
+            Console.WriteLine("This is an additional line");
         }
     }
 }
